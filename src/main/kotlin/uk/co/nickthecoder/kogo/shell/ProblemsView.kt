8013bd62b86ec79decb0ebf394acde08b8f56d91
Nick Robinson <nickthecoder@gmail.com>
2017-07-16 16:26:56 +0100

    Oops, one of the kotlin files had the "readme" pasted in it by mistake. Damn IntelliJ's auto save!

--- src/main/kotlin/uk/co/nickthecoder/kogo/shell/ProblemsView.kt
+++ src/main/kotlin/uk/co/nickthecoder/kogo/shell/ProblemsView.kt
@@ -12,37 +12,8 @@ class ProblemsView(mainWindow: MainWindow, val directory: File) : GridView(mainW
 
     override val title = directory.name
 
-    override val buttonSize = 48
+    override val buttonSize = 48.0
 
-    Kogo is a cross-platform Go board.
-
-    You can play Go against the computer (using GNU's go AI), play a local two player game, review and edit games and hone your go skills by solving go problems.
-
-    It is written in Kotlin, which means it can run on any operating system supporting Java.
-    Current Status
-    edit
-
-    Kogo is still in the early stages of development. However, you can already...
-
-    Play against the computer (using GNU's Go AI)
-    Play locally against another person
-
-    Partially working features...
-
-    Solve Go problems
-    Load .sgf files
-    Review games
-
-    Future Goals
-    edit
-
-    Save games as .sgf files
-    Talk to various go servers, playing on-line games with other people
-    Play Go variants, such as "Hidden Move Go" and "One Colour Go"
-    A version of Kogo for Android tablets (Kogo will never support Apple tablets)
-========
-    http://nickthecoder.co.uk/wiki/view/software/Kogohttp://nickthecoder.co.uk/wiki/view/software/Kogo .0
-    https://github.com/nickthecoder/kogo.githttps://github.com/nickthecoder/kogo.git
     override val viewStyle = "problems"
 
     override fun addButtons() {
